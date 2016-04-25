2ccc1d308aebf688ca6a2f9bc42189bc789f2f75
rickychang <ricky.m.chang@gmail.com>
2016-04-25 11:21:55 -0400

    adding shape dropping code

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var LINEWIDTH = 4;
 var ROWCOUNT = 20;
 var COLCOUNT = 10;
 
-// I , L, Backwards L, S, Z, Square, T
+// I , L, J, S, Z, O, T
 var SHAPE_ROTATIONS = [
     // I
     [
@@ -48,7 +48,7 @@ var SHAPE_ROTATIONS = [
             [0, 0, 0, 0]
         ]
     ],
-    // Backwards L
+    // J
     [
         [
             [0, 0, 0, 0],
@@ -75,7 +75,40 @@ var SHAPE_ROTATIONS = [
             [0, 0, 0, 0]
         ]
     ]
-]
+];
+
+var LEVEL_FRAMES_PER_DROP = {
+    0: 48,
+    1: 43,
+    2: 38,
+    3: 33,
+    4: 28,
+    5: 23,
+    6: 18,
+    7: 13,
+    8: 8,
+    9: 6,
+    10: 5,
+    11: 5,
+    12: 5,
+    13: 4,
+    14: 4,
+    15: 4,
+    16: 3,
+    17: 3,
+    18: 3,
+    19: 2,
+    20: 2,
+    21: 2,
+    22: 2,
+    23: 2,
+    24: 2,
+    25: 2,
+    26: 2,
+    27: 2,
+    28: 2,
+    29: 1
+};
 
 var currentShape;
 
@@ -84,9 +117,9 @@ var levelColors = [
     ['cyan', 'brown', 'yellow']
 ];
 
-var currentLevel = 1;
+var currentLevel = 5;
 
-var ctx;
+var ctx, gameCanvas;
 
 var board = new Array(COLCOUNT);
 for (var i = 0; i < COLCOUNT; i++) {
@@ -125,7 +158,6 @@ Shape.prototype.rotateClockwise = function() {
 Shape.prototype.rotateCounterClockwise = function() {
     var newRotation = this.currentRotation - 1;
     if (newRotation == -1) { newRotation += this.rotations.length; }
-    console.log('rotation: ' + newRotation);
     if (this.isValidPosition(this.originX, this.originY, newRotation)) {
         this.currentRotation = newRotation;
     }
@@ -151,6 +183,14 @@ Shape.prototype.moveDown = function() {
     console.log('move down');
 };
 
+Shape.prototype.dropCell = function() {
+    var newOriginY = this.originY + 1;
+
+    if (this.isValidPosition(this.originX, newOriginY, this.currentRotation)) {
+        this.originY = newOriginY;
+    }
+};
+
 Shape.prototype.getSquares = function(originX, originY, rotation) {
     if (originX === undefined) { originX = this.originX; }
     if (originY === undefined) { originY = this.originY; }
@@ -171,7 +211,6 @@ Shape.prototype.getSquares = function(originX, originY, rotation) {
 
 Shape.prototype.isValidPosition = function(newOriginX, newOriginY, newRotation) {
     var squaresToOccupy = this.getSquares(newOriginX, newOriginY, newRotation);
-    console.log(squaresToOccupy);
     for (var i = 0; i < squaresToOccupy.length; i++) {
         var coord = squaresToOccupy[i];
         // if column index is off board, return false
@@ -199,7 +238,7 @@ function updateCanvas() {
 
     if (currentShape) {
         currentShape.getSquares().forEach(function(squareCoord) {
-            drawSquare(squareCoord[0], squareCoord[1], levelColors[currentLevel][currentShape.colorIndex]);
+            drawSquare(squareCoord[0], squareCoord[1], levelColors[currentLevel % levelColors.length][currentShape.colorIndex]);
         })
     }
 }
@@ -231,10 +270,15 @@ window.onload = function() {
     });
 
 
-    var gameCanvas = document.getElementById('gameCanvas');
+    gameCanvas = document.getElementById('gameCanvas');
     ctx = gameCanvas.getContext('2d');
     currentShape = new Shape(0, 0, Math.floor(Math.random()*3));
-
-    timer = setInterval(function() { updateCanvas() }, 15);
+    var frameCount = 0;
+    timer = setInterval(function() {
+        frameCount++;
+        updateCanvas();
+        if (frameCount % LEVEL_FRAMES_PER_DROP[currentLevel] == 0) {
+            currentShape.dropCell();
+        }
+    }, 17);
 };
-
